8b56a976b2fb6745fbd08a2d216855f7f0105f90
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2025-04-16 13:24:06 -0700

    make dispenser autoenable not require reducing to one shot

--- cronjobs/dispenser_enable_submit.py
+++ cronjobs/dispenser_enable_submit.py
@@ -1,13 +1,19 @@
 from pathlib import Path
+import time
 
 import runmanager.remote as rr
 
 current_file = rr.get_labscript_file()
 
 rr.set_labscript_file(Path(__file__).with_name('dispenser_enable.py'))
-rr.set_globals({ 'repetition_index': 1 })
-if rr.n_shots() != 1:
-    raise RuntimeError()
+
+n_shots = rr.n_shots()
 rr.engage()
+
+# only need to queue up one shot, and definitely don't want to queue up a bunch
+# so just abort shot compilation if we're running more
+if n_shots > 1:
+    time.sleep(0.2)  # tested 2025-04-16, queues up three shots
+    rr.abort()
 
 rr.set_labscript_file(current_file)
