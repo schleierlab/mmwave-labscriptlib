a1521974bd5798778d1299eb6dfaee7a8344bd36
sslabstanford <40874785+sslabstanford@users.noreply.github.com>
2025-05-05 14:32:20 -0700

    regular change

--- connection_table.py
+++ connection_table.py
@@ -210,11 +210,11 @@ class LabDevices():
         )
 
         # Dummy digital out to keep even number for Blacs
-        # self.digital_out_ch22 = DigitalOut(
-        #     name='digital_out_ch22',
-        #     parent_device=ni_6363_0,
-        #     connection='port0/line22',
-        # )
+        self.digital_out_ch22 = DigitalOut(
+            name='digital_out_ch22',
+            parent_device=ni_6363_0,
+            connection='port0/line22',
+        )
 
         self.mmwave_switch = DigitalOut(
             name='mmwave_switch',
@@ -462,12 +462,12 @@ class LabDevices():
         #     BIAS_port=54321,
         # )
 
-        self.manta419b_tweezer = Manta419B(
-            'manta419b_tweezer',
-            parent_device=ni_6363_0,
-            connection="port0/line13",
-            BIAS_port=54324,
-        )
+        # self.manta419b_tweezer = Manta419B(
+        #     'manta419b_tweezer',
+        #     parent_device=ni_6363_0,
+        #     connection="port0/line13",
+        #     BIAS_port=54324,
+        # )
 
         # self.manta419b_blue_laser = Manta419B(
         #     'manta419b_blue_laser',
